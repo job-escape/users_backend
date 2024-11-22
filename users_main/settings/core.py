5e72a5b700af238ce6b2bfd9d6989a5085d140f7
myrza-saiyngali <159141235+myrza-saiyngali@users.noreply.github.com>
2024-11-22 11:20:05 +0500

    fix: settings

--- users_main/settings/core.py
+++ users_main/settings/core.py
@@ -132,6 +132,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework_simplejwt',
     'drf_spectacular',
     'polymorphic',
     'nested_admin',
